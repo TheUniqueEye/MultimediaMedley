2e03652966f029a0148a47297b4723c96cfb6713
TheUniqueEye <theuniqueeye@gmail.com>
2017-04-25 17:30:28 -0700

    initial commit

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/JingYan/masterdetaildemo/FragmentFactory.java
@@ -0,0 +1,26 @@
+package edu.ucsb.cs.cs190i.JingYan.masterdetaildemo;
+
+/**
+ * Created by Samuel on 4/20/2017.
+ */
+
+public class FragmentFactory {
+    public static SavableFragment createFragment(String fragment) {
+        switch (fragment) {
+            case "Example":
+                return new ExampleFragment();
+            case "Speech-to-Text":
+                return new ExampleFragment();
+            case "Text-to-Speech":
+                return new ExampleFragment();
+            case "Comic":
+                return new ExampleFragment();
+            case "Video":
+                return new ExampleFragment();
+            case "Animation":
+                return new ExampleFragment();
+            default:
+                return null;
+        }
+    }
+}
